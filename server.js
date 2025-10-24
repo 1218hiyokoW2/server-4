9b863cfe2ffd4ad5512045f0a03eed3aa0ff240b
1218hiyokoW2 <192060988+1218hiyokoW2@users.noreply.github.com>
2025-10-24 12:08:10 +0900

    js追記

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serverStatic } from 'jsr:@hono/hono/deno';
+
+const app = new Hono();
+
+app.use('/*', serverStatic({ root: './public' }));
+
+app.get('api', async (c) => {
+  const res = await fetch('https://randomuser.me/api/');
+  const data = await res.json();
+
+  return c.json({ message: data });
+});
+
+Deno.sever(app.fetch);
